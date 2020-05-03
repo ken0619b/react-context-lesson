c398a48439f382f7c73fbf63058af5914f1b7231
Kenichi Ukai <ken151102@gmail.com>
2020-05-03 17:13:49 +0900

    use ContextAPI

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext)
+  const collection = collections[match.params.collectionId]
   const { title, items } = collection;
   return (
     <div className='collection-page'>
@@ -21,8 +22,47 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
 
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
+
+// Context APIの使い方は以下の2通りある
+
+// #1 
+// const CollectionPage = ({ match }) => {
+//   return (
+//     <CollectionsContext.Consumer>
+//       {(collections) => {
+//         const collection = collections[match.params.collectionId];
+//         const { title, items } = collection;
+//         return (
+//           <div className="collection-page">
+//             <h2 className="title">{title}</h2>
+//             <div className="items">
+//               {items.map((item) => (
+//                 <CollectionItem key={item.id} item={item} />
+//               ))}
+//             </div>
+//           </div>
+//         );
+//       }}
+//     </CollectionsContext.Consumer>
+//   );
+// };
+
+
+// #2
+// const CollectionPage = ({ match }) => {
+//   const collections = useContext(CollectionsContext)
+//   const collection = collections[match.params.collectionId]
+//   const { title, items } = collection;
+//   return (
+//     <div className='collection-page'>
+//       <h2 className='title'>{title}</h2>
+//       <div className='items'>
+//         {items.map(item => (
+//           <CollectionItem key={item.id} item={item} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
